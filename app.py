3d556009029f876915342b94a5e4ae28587dfce8
Mading8867 <mayen8867@gmail.com>
2026-09-02 09:41:16 +0300

    Add Assignment code

---
+++ app.py
@@ -0,0 +1,8 @@
+name = "Alex"
+age = 22
+favourite_language = "Python" #This program displays a personal introduction of the user, including their name, age, favourite programming language, and main goal for the course.
+goal = "build my own software applications"
+print(f"Hello! My name is {name}.")
+print(f"I am {age} year old.")
+print(f"My favourite programming language is {favourite_language}.")
+print(f"My main goal for this course is to {goal}.")
